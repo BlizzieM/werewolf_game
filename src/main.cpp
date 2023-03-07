48988811ae16b2e0d6b4ca2974b3fd0175c11628
Tuisku Penttila <tuisku.penttila@outlook.com>
2023-03-07 13:40:27 +0200

    First person camera working

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,9 @@
 #include <iostream>
 #include "raylib.h"
 
-
-#define MAX_COLUMNS 20
-
-const int wWidht{800};
-const int wHeight{600};
+const int wWidht{1600};
+const int wHeight{900};
+const int MAX_COLUMNS{20};
 
 
 
@@ -16,9 +14,9 @@ int main(void) {
 
     //Define the camera to look into 3D world (position, target, up vector)
     Camera camera = {0};
-    camera.position = (Vector3){0.0f, 2.0f, 4.0f};       //camera pos
-    camera.target = (Vector3){0.0f, 2.0f, 0.0f};         //camera look at point
-    camera.up = (Vector3){0.0f, 1.0f, 0.0f};             //camera's up vector (rotation towards target)
+    camera.position = Vector3{4.0f, 2.0f, 4.0f};      //camera pos
+    camera.target = Vector3{0.0f, 2.0f, 0.0f};      //camera look at point
+    camera.up = Vector3{0.0f, 1.0f, 0.0f};      //camera's up vector (rotation towards target)
     camera.fovy = 99.0f;                                 //Camera fov - y
     camera.projection = CAMERA_PERSPECTIVE;              //Camera projection type
 
@@ -30,15 +28,43 @@ int main(void) {
     Vector3 positions[MAX_COLUMNS] = {0};
     Color colors[MAX_COLUMNS] = {0};
 
+    for (int i = 0; i < MAX_COLUMNS; i++)
+    {
+        
+        heights[i] = (float)GetRandomValue(1, 12);
+        positions[i] = Vector3{(float)GetRandomValue(-15,15), heights[i]/2.0f, (float)GetRandomValue(-15, 15)};
+        colors[i] = Color{(unsigned char)GetRandomValue(20,255), (unsigned char)GetRandomValue(10,55), 30, 255};
+    }
+
+    SetCameraMode(camera, CAMERA_FIRST_PERSON);
+
     SetTargetFPS(60);
 
     //main game loop
     while (!WindowShouldClose())
     {
+        UpdateCamera(&camera);
+
         BeginDrawing();
         
 
             ClearBackground(RAYWHITE);
+
+            BeginMode3D(camera);
+
+                DrawPlane(Vector3{0.0f, 0.0f, 0.0f}, Vector2{32.0f, 32.0f}, LIGHTGRAY);
+                DrawCube(Vector3{-16.0f, 2.5f, 0.0f},1.0f, 5.0f, 32.0f, BLUE);
+                DrawCube(Vector3{16.0f, 2.5, 0.0f}, 1.0f, 5.0f, 32.0f, LIME);
+                DrawCube(Vector3{0.0f, 2.5f,16.0f}, 32.0f, 5.0f, 1.0f, GOLD);
+                DrawCube(Vector3{0.0f, 2.5f,-16.0f}, 32.0f, 5.0f, 1.0f, MAGENTA);
+
+              for (int i = 0; i < MAX_COLUMNS; i++)
+                {
+                    DrawCube(positions[i], 2.0f, heights[i], 2.0f, colors[i]);
+                    DrawCubeWires(positions[i], 2.0f, heights[i], 2.0f, MAROON);
+                }
+                
+                EndMode3D();
 
             DrawText("Text", wWidht/2 -20, wHeight/2 -20, 20, LIGHTGRAY);
 
